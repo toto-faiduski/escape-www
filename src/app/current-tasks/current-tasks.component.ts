23c683184b00cce15abbb7c99b681b080ba2bb38
david <david.ruggeri@tagginfo.com>
2019-12-02 18:52:22 +0100

    Rafraichissement périodique des grilles.

--- src/app/current-tasks/current-tasks.component.ts
+++ src/app/current-tasks/current-tasks.component.ts
@@ -13,14 +13,29 @@ export class CurrentTasksComponent implements OnInit {
   currentTasks:CurrentTask[];
   error="";
   page=4;
+  id:any;
 
   constructor(
     private currentTasksService: CurrentTasksService
   ) { }
 
   ngOnInit() {
+    /*this.currentTasksService.getCurrentTasks()
+      .subscribe((data: CurrentTask[]) => this.currentTasks = data,error => this.error = error);*/
+
+    this.myInit();
+    this.id = setInterval(() => {
+      this.myInit(); 
+    }, 5000);
+  }
+
+  ngOnDestroy() {
+    if (this.id) {
+      clearInterval(this.id);
+  }
+}
+  myInit() {
     this.currentTasksService.getCurrentTasks()
       .subscribe((data: CurrentTask[]) => this.currentTasks = data,error => this.error = error);
   }
-
 }
--- src/app/recent-tasks/recent-tasks.component.ts
+++ src/app/recent-tasks/recent-tasks.component.ts
@@ -11,12 +11,28 @@ export class RecentTasksComponent implements OnInit {
   recentTasks:Task[];
   error="";
   page=1;
+  id:any;
 
   constructor(
     private recentTasksService: RecentTasksService
   ) { }
 
   ngOnInit() {
+    /*this.recentTasksService.getRecentTasks()
+      .subscribe((data: Task[]) => this.recentTasks = data,error => this.error = error);*/
+
+    this.myInit();
+    this.id = setInterval(() => {
+      this.myInit(); 
+    }, 5000);
+  }
+
+  ngOnDestroy() {
+    if (this.id) {
+      clearInterval(this.id);
+  }
+}
+  myInit() {
     this.recentTasksService.getRecentTasks()
       .subscribe((data: Task[]) => this.recentTasks = data,error => this.error = error);
   }
